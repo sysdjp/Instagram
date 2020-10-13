687c46dff231681b37ea7bf690ca32226a23586b
masakazu.yahata <sysdjp@gmail.com>
2020-10-13 16:37:23 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by ヤハタ・マサカズ on 2020/10/13.
+//  Copyright © 2020 masakazu.yahata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
