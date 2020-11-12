91d7352c09eebb31bf091255b5ca2741e43f4c29
masakazu.yahata <sysdjp@gmail.com>
2020-11-12 15:46:38 +0900

    11/12 third time

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by ヤハタ・マサカズ on 2020/11/12.
+//  Copyright © 2020 masakazu.yahata. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
