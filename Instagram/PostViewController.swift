8fa6382f070c4426ba0acd0774e48215b1654069
masakazu.yahata <sysdjp@gmail.com>
2020-11-23 22:39:43 +0900

    11/22 1回目　swif file →cocoa touch file に変更

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -2,12 +2,29 @@
 //  PostViewController.swift
 //  Instagram
 //
-//  Created by ヤハタ・マサカズ on 2020/11/06.
+//  Created by ヤハタ・マサカズ on 2020/11/23.
 //  Copyright © 2020 masakazu.yahata. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class PostViewController {
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
