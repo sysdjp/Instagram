4ada979d49a4d94569c9602e3e930c366d7f003a
masakazu.yahata <sysdjp@gmail.com>
2020-12-01 17:27:44 +0900

    12/01 1回目

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by ヤハタ・マサカズ on 2020/12/01.
+//  Copyright © 2020 masakazu.yahata. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
