cf0bc48a9b900285eff60f793b4b9fd0d9cef907
masakazu.yahata <sysdjp@gmail.com>
2020-12-08 15:23:56 +0900

    12/08 1回目

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -15,7 +15,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var mailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var displayNameTextField: UITextField!
-
+    
     // ログインボタンをタップしたときに呼ばれるメソッド
     @IBAction func handleLoginButton(_ sender: Any) {
     
